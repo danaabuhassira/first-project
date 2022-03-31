931d6bb2a978ae6b5d631c1fccf0f25e9e078041
danaabuhassira <danaabuhassira@gmail.com>
2022-03-31 22:46:13 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,40 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about',function(){
+    $name='DANA';
+    $age=21;
+   return view ('about' , compact ('name' , 'age'));
+});
+
+Route::get('/tasks',function(){
+    $tasks=[
+        'first-task' => 'Task1',
+        'second-task' => 'Task2',
+        'third-task' => 'Task3',
+    ];
+    return view('tasks',compact('tasks'));
+});
+
+Route::get('show/{id}',function($id){
+    $tasks=[
+        'first-task' => 'Task1',
+        'second-task' => 'Task2',
+        'third-task' => 'Task3',
+    ];
+    $task = $tasks[$id];
+    return view('show',compact('task' , 'id'));
+});
+
+Route::get('/contact_us',function(){
+    return view('contact_us');
+});
+
+Route::post('/contact_us',function(){
+    $name=$_POST['name'];
+    return view('contact_us',compact('name'));
 });
